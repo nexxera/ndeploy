30fdc32ee8229c8ca98f2f8c5f762bf71b1dd1c3
Martim Nascimento <martim.nascimento@nexxera.com>
2016-07-28 13:59:30 -0300

    ajustando instalação do ndeploy. Para instalar basta executar 'pip install <url-repo>'

--- pavement.py
+++ pavement.py
@@ -1,14 +1,10 @@
-import pip
 from paver.setuputils import setup
-from pip.req import parse_requirements
 from pkg_resources import iter_entry_points
 
-install_reqs = parse_requirements('requirements-dev.txt', session=pip.download.PipSession())
-requirements = [str(ir.req) for ir in install_reqs]
 setup(
     name='ndeploy',
-    packages=['ndeploy','supported_paas'],
-    install_requires=requirements,
+    packages=['ndeploy','supported_paas','ndeploy_cli'],
+    install_requires=["click"],
     dependency_links=[],
     version='0.0.1',
     url='http://www.nexxera.com/',
@@ -16,7 +12,7 @@ setup(
     author_email='isaac.souza@nexxera.com',
     entry_points={
         'console_scripts': [
-            'ndeploy = ndeploy_click:main',
+            'ndeploy = ndeploy_cli.ndeploy_click:ndeploy',
         ],
     }
 )
