790479328bcea4e4d2eeff848339fa9c33bbb636
ivanilson.zapelini <ivanilson.zapelini@nexxera.com>
2016-11-03 14:16:10 -0200

    Adicionado testes para o comando git push.

--- ndeploy/git_exec.py
+++ ndeploy/git_exec.py
@@ -17,6 +17,9 @@ class Progress(git.RemoteProgress):
 
 
 class GitExec:
+    """
+    Classe responsável por executar os comandos git
+    """
     REMOTE_REJECTED = 16
 
     def __init__(self):
@@ -24,15 +27,32 @@ class GitExec:
 
     @staticmethod
     def remote_git_add(repo_full_path, remote_name, remote_repo):
+        """
+        Adiciona um repositório remoto no repositório local
+
+        Args:
+            repo_full_path: diretório completo do repositório git
+            remote_name: nome do repositório remote
+            remote_repo: endereço do repositório remoto
+        """
         repo = git.Repo(repo_full_path)
         return repo.create_remote(remote_name, remote_repo)
 
-    def git_push(self, repo_full_path, repo_remote_name, branch_local_name, branch_remote_name):
+    def git_push(self, repo_full_path, remote_name, branch_local_name, branch_remote_name):
+        """
+        Push para o repositório remoto
+
+        Args:
+            repo_full_path: diretório completo do repositório git
+            remote_name: nome do repositório remoto
+            branch_local_name: nome da branch local
+            branch_remote_name: nome da branch remota
+        """
         repo = git.Repo(repo_full_path)
-        repo_remote = repo.remote(repo_remote_name)
+        repo_remote = repo.remote(remote_name)
         ref_spec = "{branch_local}:{branch_remote}".format(branch_local=branch_local_name, branch_remote=branch_remote_name)
         push_info = repo_remote.push(ref_spec, progress=self.progress)[0]
 
         if push_info.flags & GitExec.REMOTE_REJECTED:
             raise GitExecError("Failed push for {remote_repo} repo full path {repo_full_path}"
-                               .format(remote_repo=repo_remote_name, repo_full_path=repo_full_path))
+                               .format(remote_repo=remote_name, repo_full_path=repo_full_path))
